1b90b90b8b43c4861b35b0fc1ffbb94f4a6fdefe
TamilRams <126973710+TamilRams@users.noreply.github.com>
2023-03-04 21:43:12 +0530

    Add files via upload

---
+++ day1/LearningDataType.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class LearningDataType {
+	public static void main(String[] args) {
+		
+		int classno = 1;
+		char section = 'A';
+		boolean newstudent = true;
+		float percentage = 80.5f;
+		String studentName = "Yazhini";
+		long mobileno = 1234567890l;
+		
+		System.out.println("Student name : "+studentName);
+		System.out.println("Is she new Student : "+newstudent);
+		System.out.println("Class of the student : "+classno);
+		System.out.println("What section she is : "+section);
+		System.out.println("Mark scored : "+percentage);
+		System.out.println("student Mobileno : "+mobileno);
+		
+		
+	}
+
+}
